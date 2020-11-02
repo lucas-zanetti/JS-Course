6c0fa508a213950bff24db4b639f976b78b2bc3a
lucas-zanetti <lucas.hzanetti@gmail.com>
2020-11-01 22:49:34 -0300

    Adding keyboard events

--- curso-javascript-projeto-calculadora-clone/scripts/controller/CalcController.js
+++ curso-javascript-projeto-calculadora-clone/scripts/controller/CalcController.js
@@ -11,6 +11,7 @@ class CalcController{
         this._currentDate;
         this.initialize();
         this.initButtonsEvents();
+        this.initKeyboard();
     }
 
     initialize(){
@@ -19,6 +20,46 @@ class CalcController{
             this.setDisplayDateTime();
         },1000);
         this.setLastNumberToDisplay();
+    }
+
+    initKeyboard(){
+        document.addEventListener('keyup', e=>{
+            switch(e.key){
+                case 'Escape':
+                    this.clearAll();
+                    break;
+                case 'Backspace':
+                    this.clearEntry();
+                    break;
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '%':
+                    this.addOperation(e.key);
+                    break;
+                case '.':
+                case ',':
+                    this.addDot();
+                    break;
+                case 'Enter':
+                case '=':
+                    this.calc();
+                    break;
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                case '0':
+                    this.addOperation(parseInt(e.key));
+                    break;                
+            }
+        });
     }
 
     addEventListenerAll(element, events, fn){
