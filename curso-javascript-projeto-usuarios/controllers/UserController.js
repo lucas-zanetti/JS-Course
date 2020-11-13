a612568a922d1a3b80733e95554cd125b07e0aca
Lucas-Zanetti <lucas.hzanetti@gmail.com>
2020-11-13 17:11:00 -0300

    Adding FileReader to load photo and add to users table

--- curso-javascript-projeto-usuarios/controllers/UserController.js
+++ curso-javascript-projeto-usuarios/controllers/UserController.js
@@ -9,13 +9,33 @@ class UserController {
     onSubmit(){
         this.formEl.addEventListener('submit', event => {
             event.preventDefault();
-            this.addLine(this.getValues());
+            let values = this.getValues();
+            this.getPhoto((content) => {
+                values.photo = content;
+                this.addLine(values);
+            });
         });
+    }
+
+    getPhoto(callback){
+        let fileReader = new FileReader();
+        let elements = [...this.formEl.elements].filter(item =>{
+            if (item.name === "photo")
+                return item;
+        });
+
+        let file = elements[0].files[0];
+
+        fileReader.onload = () => {
+            callback(fileReader.result);
+        };
+
+        fileReader.readAsDataURL(file);
     }
 
     getValues(){
         let user = {};
-        [...this.formEl.elements].forEach((field, index)=>{
+        [...this.formEl.elements].forEach((field)=>{
             if(field.name=="gender"){
                 if (field.checked){
                     user[field.name] = field.value;
@@ -29,7 +49,7 @@ class UserController {
 
     addLine(dataUser){
         this.tableEl.innerHTML = `<tr>
-                            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                            <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
                             <td>${dataUser.name}</td>
                             <td>${dataUser.email}</td>
                             <td>${dataUser.admin}</td>
