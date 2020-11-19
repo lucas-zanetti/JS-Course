e982d8982717d3ff2f08376046d9c0f6e6ebcc33
lucas-zanetti <lucas.hzanetti@gmail.com>
2020-11-18 23:07:57 -0300

    Fixing no picture bug and changing admin column view

--- curso-javascript-projeto-usuarios/controllers/UserController.js
+++ curso-javascript-projeto-usuarios/controllers/UserController.js
@@ -40,17 +40,23 @@ class UserController {
                 reject(e);
             };
 
-            fileReader.readAsDataURL(file);
+            if(file){
+                fileReader.readAsDataURL(file);
+            } else {
+                resolve('dist/img/boxed-bg.jpg');
+            }     
         });        
     }
 
     getValues(){
         let user = {};
         [...this.formEl.elements].forEach((field)=>{
-            if(field.name=="gender"){
+            if(field.name == "gender"){
                 if (field.checked){
                     user[field.name] = field.value;
                 }
+            } else if (field.name == "admin"){
+                user[field.name] = field.checked;
             } else {
                 user[field.name] = field.value;
             }    
@@ -59,17 +65,22 @@ class UserController {
     }
 
     addLine(dataUser){
-        this.tableEl.innerHTML = `<tr>
+
+        let tr = document.createElement('tr');
+
+        tr.innerHTML = `
                             <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
                             <td>${dataUser.name}</td>
                             <td>${dataUser.email}</td>
-                            <td>${dataUser.admin}</td>
+                            <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
                             <td>${dataUser.birth}</td>
                             <td>
-                            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                             </td>
-                        </tr>`;    
+                        `;
+
+        this.tableEl.appendChild(tr);
     }
 
 }
