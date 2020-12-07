bfa4cb93cebc48c051ee1ba54ec4aee9003a62b3
Lucas.Zanetti <lucas.zanetti@sovos.com>
2020-12-07 18:12:05 -0300

    Adding edit user form and start the user edition

--- curso-javascript-projeto-usuarios/controllers/UserController.js
+++ curso-javascript-projeto-usuarios/controllers/UserController.js
@@ -4,6 +4,13 @@ class UserController {
         this.formEl = document.getElementById(formId);
         this.tableEl = document.getElementById(tableId);
         this.onSubmit();
+        this.onEdit();
+    }
+
+    onEdit(){
+        document.querySelector("#box-user-update .btn-cancel").addEventListener("click", e=>{
+            this.showPanelCreate();
+        });
     }
 
     onSubmit(){
@@ -90,10 +97,23 @@ class UserController {
                             <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
                             <td>${Utils.dateFormat(dataUser.register)}</td>
                             <td>
-                                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                                <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
                                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                             </td>
                         `;
+
+        tr.querySelector(".btn-edit").addEventListener("click", e=>{
+            let json = JSON.parse(tr.dataset.user);
+            let form = document.querySelector("#form-user-update");
+            for (let name in json){
+                let field = form.querySelector("[name="+ name.replace("_","") + "]");
+                if (field){
+                    if (field.type == "file") continue;
+                    field.value = json[name];
+                }
+            }
+            this.showPanelUpdate();
+        });
 
         this.tableEl.appendChild(tr);
 
@@ -110,6 +130,16 @@ class UserController {
         });
         document.querySelector("#number-users").innerHTML = numberUsers;
         document.querySelector("#number-users-admin").innerHTML = numberAdmin;
+    }
+
+    showPanelCreate(){
+        document.querySelector("#box-user-create").style.display = "block";
+        document.querySelector("#box-user-update").style.display = "none";
+    }
+
+    showPanelUpdate(){
+        document.querySelector("#box-user-create").style.display = "none";
+        document.querySelector("#box-user-update").style.display = "block";
     }
 
 }
