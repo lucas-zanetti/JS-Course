46bd89a79ce26c56fd204669ae343921e07def74
lucas-zanetti <lucas.hzanetti@gmail.com>
2020-12-13 17:59:11 -0300

    Adding info load on editing

--- curso-javascript-projeto-usuarios/controllers/UserController.js
+++ curso-javascript-projeto-usuarios/controllers/UserController.js
@@ -109,7 +109,19 @@ class UserController {
                 let field = form.querySelector("[name="+ name.replace("_","") + "]");
                 if (field){
                     if (field.type == "file") continue;
-                    field.value = json[name];
+                    switch(field.type){
+                        case 'file':
+                            continue;
+                        case 'radio':
+                            field = form.querySelector("[name="+ name.replace("_","") + "][value="+json[name]+"]");
+                            field.checked = true;
+                            break;
+                        case 'checkbox':
+                            field.checked = json[name];
+                            break;
+                        default:
+                            field.value = json[name];
+                    }
                 }
             }
             this.showPanelUpdate();
